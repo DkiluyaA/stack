dc881bbca39b7059c177fe04790c7b2f5a982137
Alina Damnytska <alina.damnytska@wesoftyou.com>
2021-12-14 16:58:09 +0200

    generate stack

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+   const getRandomInt = (max: number = 1000 , from: number = 0) => {
+    return Math.floor(Math.random() * max) + from;
+  }
+
+
+  const initStack = () => {
+     const dataStack: number[] = [];
+
+      return () =>{
+          dataStack.push(getRandomInt(100,0));
+       return (
+          <div className="wrapper-stack">
+            {dataStack.map(item => <div className="item-stack">{item}</div>)}
+          </div> )
+       }
+  }
+
+
+    const getStack =  (action: string): any => {
+        const sizeStack = getRandomInt(8,3);
+        const stack = initStack();
+        const stacks  = [];
+
+        for(let i = 0; i < sizeStack; i++){
+            stacks.push(stack());
+        }
+         return stacks;
+   }
+
+
+    return (
+    <div className="App">
+        {getStack('push')}
+    </div>
+  );
+}
+
+export default App;
